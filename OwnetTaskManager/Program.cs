69d2c7a75aeddc38d5608a811dec817f972957b2
limematheuz <mribeiro17.info@gmail.com>
2024-10-08 01:29:47 +0200

    add new keyvault config on program file

--- OwnetTaskManager/Program.cs
+++ OwnetTaskManager/Program.cs
@@ -1,5 +1,7 @@
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
 using OwnetTaskManager.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,8 +10,31 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+if (builder.Environment.IsProduction())
+{
+    var keyVaultURL = builder.Configuration.GetSection("KeyVault:keyVaultURL");
+    var keyVaultClientId = builder.Configuration.GetSection("KeyVault:keyVaultClientId");
+    var keyVaultClientSecret = builder.Configuration.GetSection("KeyVault:keyVaultClientSecret");
+    var keyVaultDirectoryID = builder.Configuration.GetSection("KeyVault:keyVaultDirectoryID");
+
+    var credential = new ClientSecretCredential(keyVaultClientId.Value!.ToString(),
+        keyVaultClientSecret.Value!.ToString(), keyVaultDirectoryID.Value!.ToString());
+
+    builder.Configuration.AddAzureKeyVault(keyVaultURL.Value!.ToString(), keyVaultClientSecret.Value!.ToString(),
+        keyVaultClientId.Value!.ToString(), new DefaultKeyVaultSecretManager());
+
+    var client = new SecretClient(new Uri(keyVaultURL.Value!.ToString()), credential);
+
+    builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseSqlServer(client.GetSecret("ProductionConnection").Value.Value).ToString());
+}
+
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+}
 
 
 var app = builder.Build();
